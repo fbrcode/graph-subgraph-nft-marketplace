d967efac1203ebf3aa39546c4d2dc6b559ce1dc9
fbrcode <fabio.bressler@gmail.com>
2022-08-03 14:53:48 -0300

    cleanup event mappings

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,55 +1,11 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  NftMarketplace,
-  ItemBought,
-  ItemCancelled,
-  ItemListed
-} from "../generated/NftMarketplace/NftMarketplace"
-import { ExampleEntity } from "../generated/schema"
+  ItemBought as ItemBoughtEvent,
+  ItemCancelled as ItemCancelledEvent,
+  ItemListed as ItemListedEvent,
+} from "../generated/NftMarketplace/NftMarketplace";
 
-export function handleItemBought(event: ItemBought): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleItemBought(event: ItemBoughtEvent): void {}
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleItemCancelled(event: ItemCancelledEvent): void {}
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.buyer = event.params.buyer
-  entity.nftAddress = event.params.nftAddress
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.getListing(...)
-  // - contract.getProceeds(...)
-}
-
-export function handleItemCancelled(event: ItemCancelled): void {}
-
-export function handleItemListed(event: ItemListed): void {}
+export function handleItemListed(event: ItemListedEvent): void {}
